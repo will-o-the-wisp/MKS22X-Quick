95195dc9e469de247637d36c99c51e79403d1dc0
will-o-the-wisp <kli18@stuy.edu>
2019-03-14 11:06:08 -0400

    tested quicksort with new driver

--- Quick.java
+++ Quick.java
@@ -1,18 +1,39 @@
 import java.util.*;
 public class Quick{
-  public static void main(String[] args){
-    Random r = new Random();
-    int[] a = new int[(int)(1e6)];
-    for(int i=0;i<a.length;i++){
-      a[i]=r.nextInt();
+  public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
     }
-    if(args.length>0){
-      quicksort(a);
-    }
-    else{
-      Arrays.sort(a);
-    }
+    System.out.println();
   }
+}
 
 /*Modify the array such that:
  *1. Only the indices from start to end inclusive are considered in range
